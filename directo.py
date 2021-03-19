bd2d8c1595472df21b22c3bbc539aa49a045a3a1
Juan Jose <junnior25a@hotmail.com>
2021-03-19 00:10:10 -0500

    Interfaz final de la directa

--- directo.py
+++ directo.py
@@ -2,39 +2,69 @@ import numpy as np
 import math
 class directo():
     teta1=0
-    d1=0
-    a2=0
     teta3=0
     teta4=0
     teta5=0
     teta2=0
     teta6=0
-    a5=0
-    d4=0
+    lo=5
+    l6=5
+    d2=0
+    l4=0
     d3=0
-    d6=0
+    d5=0
+    a1=0
+    a2=0
+    a3=0
+    a4=0
+    a5=0
+    a6=0
+    alfa1=0
+    alfa2=0
+    alfa3=0
+    alfa4=0
+    alfa5=0
+    alfa6=0
     sol=[]
-    def __init__(self,t1,t4,t5,t6,dis1,dis3,dis6):
-    
+    def __init__(self,t1,t2,t3,t4,t5,t6,dis1,dis2,dis3,dis4,dis5,dis6,a1,a2,a3,a4,a5,a6
+    ,alfa1,alfa2,alfa3,alfa4,alfa5,alfa6):
         self.teta1=float(t1)
-        self.teta4=float(t4)
-        self.teta5=float(t5)
+        self.teta2=float(t2)
+        self.teta3=float(t3)
+        self.teta4=float(t4)+180
+        self.teta5=float(t5)+180
         self.teta6=float(t6)
         self.d1=float(dis1)
+        self.d2=float(dis2)
         self.d3=float(dis3)
+        self.l4=float(dis4)
+        self.d5=float(dis5)
         self.d6=float(dis6)
+        self.a1=float(a1)
+        self.a2=float(a2)
+        self.a3=float(a3)
+        self.a4=float(a4)
+        self.a5=float(a5)
+        self.a6=float(a6)
+        self.alfa1=float(alfa1)
+        self.alfa2=float(alfa2)
+        self.alfa3=float(alfa3)
+        self.alfa4=float(alfa4)
+        self.alfa5=float(alfa5)
+        self.alfa6=float(alfa6)
+        #self.d6=float(dis6)
         self.matrices()
         
     def matrices(self):
         A01=np.array([[ math.cos(math.radians(self.teta1)), -math.sin(math.radians(self.teta1)),0,0],
         [ math.sin(math.radians(self.teta1)),math.cos(math.radians(self.teta1)),0,0],
-        [ 0, 0, 1,self.d1],
+        [ 0, 0, 1,self.lo],
         [ 0, 0,0,1]])
         print(A01)
         
         A12=np.array([[ 0, 0, 1,0],
         [ 1,0,0,0],
-        [0,1,0,0],
+        [0,1,0,self.d2],
         [0,0,0,1]])
 
         A23=np.array([[ 0, -1, 0,0],
@@ -44,7 +74,7 @@ class directo():
 
         A34=np.array([[math.cos(math.radians(self.teta4)),0,math.sin(math.radians(self.teta4)),0],
         [ math.sin(math.radians(self.teta4)),0,-math.cos(math.radians(self.teta4)),0],
-        [ 0,1,0,0],
+        [ 0,1,0,self.l4],
         [ 0, 0,0,1]])
 
         A45=np.array([[ math.cos(math.radians(self.teta5)),0,math.sin(math.radians(self.teta5)),0],
@@ -54,7 +84,7 @@ class directo():
 
         A56=np.array([[ math.cos(math.radians(self.teta6)), -math.sin(math.radians(self.teta6)),0,0],
         [ math.sin(math.radians(self.teta6)),math.cos(math.radians(self.teta6)),0,0],
-        [ 0, 0, 1,self.d6],
+        [ 0, 0, 1,self.l6],
         [ 0, 0,0,1]])
         self.multiplicacion(A01,A12,A23,A34,A45,A56)
     def multiplicacion(self,A01,A12,A23,A34,A45,A56):
@@ -65,12 +95,17 @@ class directo():
         T=np.round(np.dot(p4,A56),1)
 
         print(T)
-        print(T[0,3])
-        print(T[1,3])
-        print(T[2,3])
-
+        
+        if (T[1,3] == -0.0):
+            T[1,3]=0
+        self.sol.clear()
+        
         self.sol.append(T[0,3])
         self.sol.append(T[1,3])
         self.sol.append(T[2,3])
+
+        print(T[0,3])
+        print(T[1,3])
+        print(T[2,3])
     def __iter__(self):
         return iter(self.sol)
--- interfaz.py
+++ interfaz.py
@@ -2,12 +2,19 @@ from ventana import *
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton,QWidget
 from PyQt5.QtCore import pyqtSlot
 from directo import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import *
 #Separando la lógica del diseño
 class MainWindow (QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
         self.boton.clicked.connect(self.validarCampos)
+        self.limpiar.clicked.connect(self.limpiarCampos)
+
+        pixmap = QPixmap('robot.jpeg')
+        self.img.setPixmap(pixmap)
+        self.img.setScaledContents(True)
 
     @pyqtSlot()
     def validarCampos(self):
@@ -20,15 +27,28 @@ class MainWindow (QtWidgets.QMainWindow, Ui_MainWindow):
             QMessageBox.warning(self, "Validacion Incorrecta", "Todos los campos son Obligatorios", QMessageBox.Discard)
         else:
             
-            x,y,z=directo(self.teta1.text(),self.teta4.text(),self.teta5.text(),self.teta6.text(),
-            self.d1.text(),self.d3.text(),self.d6.text())
+            x,y,z=directo(self.teta1.text(),self.teta2.text(),self.teta3.text(),self.teta4.text(),self.teta5.text(),self.teta6.text()
+            ,self.d1.text(),self.d2.text(),self.d3.text(),self.d4.text(),self.d5.text(),self.d6.text(),
+            self.a1.text(),self.a2.text(),self.a3.text(),self.a4.text(),self.a5.text(),self.a6.text(),
+            self.alfa1.text(),self.alfa2.text(),self.alfa3.text(),self.alfa4.text(),self.alfa5.text(),self.alfa6.text()
+            )
 
             print(x,y,z)
-            
             #Mostrar las soluciones en la interfaz
+            
             self.textEdit.setText(str(x))
             self.textEdit_3.setText(str(y))
             self.textEdit_2.setText(str(z))
+    def limpiarCampos(self):
+        self.teta1.clear()
+        self.teta4.clear()
+        self.teta5.clear()
+        self.teta6.clear()
+        self.d2.clear()
+        self.d3.clear()
+        self.textEdit.clear()
+        self.textEdit_3.clear()
+        self.textEdit_2.clear()
 if __name__=="__main__":
     app=QtWidgets.QApplication([])
     window=MainWindow()
